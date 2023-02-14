8fceace83a233ff87b5ae00e510c03262b964021
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-02-14 12:22:51 +0530

    Add food and user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all.order(:id)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
