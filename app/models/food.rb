5c16b55b385573804ae857e14d448681bbff0119
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-02-16 15:03:42 +0530

    Add food page style

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,9 @@
 class Food < ApplicationRecord
+  belongs_to :user, foreign_key: 'user_id'
+
+  validates :name, :measurement_unit, :price, :quantity, presence: true
+
+
   has_many :foods_recipes
   has_many :recipes, through: :food_recipes
 end
